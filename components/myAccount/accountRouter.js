157eab5dd8b1b3f66b93541accded7d186335674
Nguyen Tuan Khoa <75377239+khoa2212@users.noreply.github.com>
2022-01-17 01:10:37 +0700

    cap nhat thong tin ca nhan

--- components/myAccount/accountRouter.js
+++ components/myAccount/accountRouter.js
@@ -4,5 +4,5 @@ var router = express.Router();
 const accountController = require('./accountController');
 
 router.get('/', accountController.showAccountLayout);
-
+router.post('/', accountController.changeAccountInfo);
 module.exports = router;
