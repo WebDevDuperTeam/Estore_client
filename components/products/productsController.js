ab8d31f162f01aa0fea1bed8e2e460547f669d01
Nguyen Tuan Khoa <75377239+khoa2212@users.noreply.github.com>
2022-01-17 15:29:33 +0700

    sap xep san pham theo gia from Minh Pham

--- components/products/productsController.js
+++ components/products/productsController.js
@@ -5,6 +5,28 @@ const e = require("express");
 //     res.render('products/productlist', {products: products});
 // }
 
+// exports.list = async(req, res) => {
+//     try {
+//         const page = !isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0;
+//
+//         const searchName = req.query.key;
+//
+//         const gt = req.query.gt;
+//         const price = req.query.price;
+//         let prices = Array(0, 1);
+//         if (price != null) {
+//             prices = price.split(',')
+//         }
+//         const mau = req.query.mau;
+//
+//         const products = await productsService.list(page, searchName, gt, prices[0] * 22000, prices[1] * 22000, mau);
+//         res.render('products/productlist', { products: products });
+//
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
 exports.list = async(req, res) => {
     try {
         const page = !isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0;
@@ -18,8 +40,42 @@ exports.list = async(req, res) => {
             prices = price.split(',')
         }
         const mau = req.query.mau;
+        const sapxep=req.query.sapxep;
 
         const products = await productsService.list(page, searchName, gt, prices[0] * 22000, prices[1] * 22000, mau);
+        if(sapxep!='0'&&sapxep!=undefined)
+        {
+            if(sapxep=='down')
+            {
+                for(let i=0;i<products.length-1;i++)
+                {
+                    for(let j=i+1;j<products.length;j++)
+                    {
+                        if(products[i].GIA<products[j].GIA)
+                        {
+                            let a=products[i];
+                            products[i]=products[j];
+                            products[j]=a;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for(let i=0;i<products.length-1;i++)
+                {
+                    for(let j=i+1;j<products.length;j++)
+                    {
+                        if(products[i].GIA>products[j].GIA)
+                        {
+                            let a=products[i];
+                            products[i]=products[j];
+                            products[j]=a;
+                        }
+                    }
+                }
+            }
+        }
         res.render('products/productlist', { products: products });
 
     } catch (err) {
