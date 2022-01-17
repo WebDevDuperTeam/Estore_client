1b17d4fe0a91c01cdbedd8645dd521fbbe0ce87e
Nguyen Tuan Khoa <75377239+khoa2212@users.noreply.github.com>
2022-01-17 09:52:40 +0700

    search san pham

--- components/products/productsService.js
+++ components/products/productsService.js
@@ -1,26 +1,91 @@
 const {models} = require('../../models');
 const {Sequelize} = require("sequelize");
-exports.list = (page = 0, itemPerPage = 9) => {
-    return models.quanao.findAll({
-        attributes: ['QUANAO_ID', 'LOAI.TEN_LOAI', 'MAU', 'GIOI_TINH', 'THUONGHIEU.TEN_THUONG_HIEU', 'LINK', 'GIA'],
-        include: [{
+// exports.list = (page = 0, itemPerPage = 9) => {
+//     return models.quanao.findAll({
+//         attributes: ['QUANAO_ID', 'LOAI.TEN_LOAI', 'MAU', 'GIOI_TINH', 'THUONGHIEU.TEN_THUONG_HIEU', 'LINK', 'GIA'],
+//         include: [{
+//                 model: models.loai,
+//                 as: "LOAI",
+//                 required: true
+//             },
+//             {
+//                 model: models.thuonghieu,
+//                 as: "THUONGHIEU",
+//                 required: true
+//             }
+//         ],
+//         where: [{
+//             DA_XOA:{[Sequelize.Op.is]: false}
+//         }],
+//         offset: page * itemPerPage,
+//         limit: itemPerPage,
+//         raw: true
+//     });
+// };
+exports.list = (page = 0, searchName, gt, price1, price2, mau, itemPerPage = 9) => {
+    if (searchName && searchName !== "")
+        return models.quanao.findAll({
+            attributes: ['QUANAO_ID', 'LOAI.TEN_LOAI', 'MAU', 'GIOI_TINH', 'THUONGHIEU.TEN_THUONG_HIEU', 'LINK', 'GIA'],
+            include: [{
                 model: models.loai,
                 as: "LOAI",
-                required: true
+                required: true,
+                where: {
+                    TEN_LOAI: {
+                        [Sequelize.Op.substring]: searchName
+                    }
+                }
             },
-            {
-                model: models.thuonghieu,
-                as: "THUONGHIEU",
-                required: true
-            }
-        ],
-        where: [{
-            DA_XOA:{[Sequelize.Op.is]: false}
-        }],
-        offset: page * itemPerPage,
-        limit: itemPerPage,
-        raw: true
-    });
+                {
+                    model: models.thuonghieu,
+                    as: "THUONGHIEU",
+                    required: true
+
+                }
+            ],
+            where: [{
+                DA_XOA: {
+                    [Sequelize.Op.is]: false
+                },
+                GIOI_TINH: gt,
+                GIA: {
+                    [Sequelize.Op.between]: [price1, price2]
+                },
+                MAU: mau
+
+
+            }],
+            offset: page * itemPerPage,
+            limit: itemPerPage,
+            raw: true
+        });
+    else
+        return models.quanao.findAll({
+            attributes: ['QUANAO_ID', 'LOAI.TEN_LOAI', 'MAU', 'GIOI_TINH', 'THUONGHIEU.TEN_THUONG_HIEU', 'LINK', 'GIA'],
+            include: [{
+                model: models.loai,
+                as: "LOAI",
+                required: true,
+
+
+            },
+                {
+                    model: models.thuonghieu,
+                    as: "THUONGHIEU",
+                    required: true
+
+                }
+            ],
+            where: [{
+                DA_XOA: {
+                    [Sequelize.Op.is]: false
+                }
+
+            }],
+            offset: page * itemPerPage,
+            limit: itemPerPage,
+            raw: true
+        });
 };
 
 exports.showDetail = (ID) => {
