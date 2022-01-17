02d6e0cc1a61413c7b5e6ef5168140fb411a65dd
Nguyen Tuan Khoa <75377239+khoa2212@users.noreply.github.com>
2022-01-17 10:30:15 +0700

    Last khoa update

--- components/products/productsController.js
+++ components/products/productsController.js
@@ -1,8 +1,30 @@
 const productsService = require('./productsService');
 const e = require("express");
-exports.list = async (req, res) => {
-    let products = await productsService.list(!isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1:0);
-    res.render('products/productlist', {products: products});
+// exports.list = async (req, res) => {
+//     let products = await productsService.list(!isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1:0);
+//     res.render('products/productlist', {products: products});
+// }
+
+exports.list = async(req, res) => {
+    try {
+        const page = !isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0;
+
+        const searchName = req.query.key;
+
+        const gt = req.query.gt;
+        const price = req.query.price;
+        let prices = Array(0, 1);
+        if (price != null) {
+            prices = price.split(',')
+        }
+        const mau = req.query.mau;
+
+        const products = await productsService.list(page, searchName, gt, prices[0] * 22000, prices[1] * 22000, mau);
+        res.render('products/productlist', { products: products });
+
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 exports.showDetail = async (req, res) => {
